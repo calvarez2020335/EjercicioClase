5864d32ba94cf4efbde76448353532b40efdebbe
Informatica Kinal <88805710+adelacruz2020390@users.noreply.github.com>
2022-03-22 17:24:59 -0600

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: 'inicio', component: InicioComponent},
   {path: 'dash', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
